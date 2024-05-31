d111882af78f30213aabdd6a43274a75bc61cdde
katann-IWATATE-tann <104120192+katann-tann@users.noreply.github.com>
2024-05-31 13:40:14 +0900

    cs0524

--- cs0524/Form1.cs
+++ cs0524/Form1.cs
@@ -6,5 +6,45 @@ namespace cs0524
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+            label1.Text = "Hello";
+            MessageBox.Show($"‚±‚ñ‚É‚¿‚Í{textBox1.Text}‚³‚ñ");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += 1;
+            label1.Top += 1;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int vx = 5;
+            MessageBox.Show($"{vx}");
+
+            vx = 10;
+            MessageBox.Show($"{vx}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show($"{vx}");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            vx += 10;
+            MessageBox.Show($"{vx}");
+        }
     }
 }
